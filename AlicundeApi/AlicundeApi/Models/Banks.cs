547617fa00df1f9ca99736e1ba96c8d7c16d6982
Cristian <cristiangmoralesa@gmail.com>
2024-09-24 14:15:58 +0200

    Add model Banks with parameters for consume Api

---
+++ AlicundeApi/AlicundeApi/Models/Banks.cs
@@ -0,0 +1,9 @@
+﻿namespace AlicundeApi.Models
+{
+    public class Banks
+    {
+        public string name { get; set; }
+        public string bic { get; set; }
+        public string country { get; set; }
+    }
+}
