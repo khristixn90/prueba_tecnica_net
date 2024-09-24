6864551c07240cd85fab60ae6b7d86a2e8fb068e
Cristian <cristiangmoralesa@gmail.com>
2024-09-24 15:24:02 +0200

    Add new packages NuGet

--- AlicundeApi/AlicundeApi/obj/Debug/net8.0/AlicundeApi.AssemblyInfo.cs
+++ AlicundeApi/AlicundeApi/obj/Debug/net8.0/AlicundeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlicundeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57f4b20f2157067e91503c1044bcc74d3efa289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70edd18c98c0cfe6dfc387420b361d53c4060de0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlicundeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlicundeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
