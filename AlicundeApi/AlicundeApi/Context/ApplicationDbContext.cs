7c9e295d8090d9bbdeecfa208bb84036a4b83da1
Cristian <cristiangmoralesa@gmail.com>
2024-09-24 14:51:33 +0200

    Add DBContext connections database

---
+++ AlicundeApi/AlicundeApi/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AlicundeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AlicundeApi.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Bank> Bank { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
